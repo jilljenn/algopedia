58fc7489540fcf3fef10f913c2339ad17f1bc51f
Marc Jeanmougin <marcjeanmougin@free.fr>
2015-12-11 15:34:40 +0100

    Add Language

--- web/algopedia/admin.py
+++ web/algopedia/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
-from algopedia.models import Algo, Implementation
+from algopedia.models import Algo, Implementation, Language
 
-admin.site.register(Algo)
-admin.site.register(Implementation)
+class AlgoAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+class ImplementationAdmin(admin.ModelAdmin):
+    list_display = ['algo', 'user', 'lang']
+
+
+admin.site.register(Algo, AlgoAdmin)
+admin.site.register(Implementation, ImplementationAdmin)
+admin.site.register(Language)
--- web/algopedia/models.py
+++ web/algopedia/models.py
@@ -6,8 +6,19 @@ class Algo(models.Model):
     name = models.CharField(max_length=42)
     description = models.TextField()
 
+    def __str__(self):
+        return self.name
+
 
 class Implementation(models.Model):
     user = models.ForeignKey(User)
     algo = models.ForeignKey('Algo')
     code = models.TextField()
+    lang = models.ForeignKey('Language')
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=42)
+
+    def __str__(self):
+        return self.name
