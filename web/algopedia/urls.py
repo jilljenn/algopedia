e3120c6bf75e2db3f1c07cdf108609a7c6b19704
Olivier Marty <olivier.marty.m@gmail.com>
2015-12-11 11:27:19 +0100

    Use url names and url reversing

--- web/algopedia/urls.py
+++ web/algopedia/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from algopedia.views import AlgoList, AlgoDetail
 
 urlpatterns = [
-    url(r'^algos/', AlgoList.as_view()),
-    url(r'^algo/(?P<pk>\d+)', AlgoDetail.as_view())
+    url(r'^algos/', AlgoList.as_view(), name="algos"),
+    url(r'^algo/(?P<pk>\d+)', AlgoDetail.as_view(), name="algo")
 ]
