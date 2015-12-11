5a9707e9f28de3278a5a6f6ccc0d8a5e8faa1517
Olivier Marty <olivier.marty.m@gmail.com>
2015-12-11 15:52:37 +0100

    Add models Category

--- web/algopedia/models.py
+++ web/algopedia/models.py
@@ -2,9 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=42)
+
+    def __str__(self):
+        return self.name
+
 class Algo(models.Model):
     name = models.CharField(max_length=42)
     description = models.TextField()
+    category = models.ManyToManyField(Category)
 
     def __str__(self):
         return self.name
