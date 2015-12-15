4ce040b5d70bbddd04bfd290ff3442aa011c9613
Olivier Marty <olivier.marty.m@gmail.com>
2015-12-15 20:10:39 +0100

    Add pygments to highlight codes

--- web/algopedia/models.py
+++ web/algopedia/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
+from pygments import highlight
+from pygments.lexers import get_lexer_by_name, guess_lexer
+from pygments.formatters import HtmlFormatter
+from pygments.util import ClassNotFound
+from pygments.lexers.special import TextLexer
 
 class Category(models.Model):
     name = models.CharField(max_length=42, unique=True)
@@ -28,6 +33,17 @@ class Implementation(models.Model):
 
     def get_absolute_url(self):
        return reverse('algopedia:implementation-detail', kwargs={'pk': self.pk})
+
+    def get_code(self):
+       try:
+           lexer = get_lexer_by_name(self.lang.name, stripall=True)
+       except ClassNotFound:
+           try:
+               lexer = guess_lexer(self.code)
+           except ClassNotFound:
+               lexer = TextLexer()
+       formatter = HtmlFormatter(linenos=True)
+       return highlight(self.code, lexer, formatter)
 
 
 class Language(models.Model):
