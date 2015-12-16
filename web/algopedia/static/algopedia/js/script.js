0c12472e65414ea54b485258cdffe5fa538cdddb
Olivier Marty <olivier.marty.m@gmail.com>
2015-12-16 11:24:27 +0100

    Add button detail in modal

--- web/algopedia/static/algopedia/js/script.js
+++ web/algopedia/static/algopedia/js/script.js
@@ -1,10 +1,11 @@
 $('#modal_implementation_detail').on('show.bs.modal', function (event) {
-  var button = $(event.relatedTarget) // Button that triggered the modal
-  var id = button.data('implementation-id') // Extract info from data-* attributes
+  var button = $(event.relatedTarget)
+  var id = button.data('implementation-id')
   var modal = $(this)
   modal.find('.modal-title .modal-title-user').html(button.data('implementation-user'))
   modal.find('.modal-title .modal-title-lang').html(button.data('implementation-lang'))
-  modal.find('.modal-body').html("<p>loading...</p>")
+  modal.find('.modal-body').html('<p>loading...</p>')
+  modal.find('.modal-footer a').attr('href', button.attr('href'))
   $.ajax('/ajax/implementation/' + id)
     .done(function(data) {
       modal.find('.modal-body').html(data)
