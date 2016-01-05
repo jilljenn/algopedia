aac976a54d9a3104d830c7dd7da27874401013af
Olivier Marty <olivier.marty.m@gmail.com>
2016-01-05 19:06:40 +0100

    Add a TeX template for notebooks and pdflatex options

--- web/algopedia/notebook.py
+++ web/algopedia/notebook.py
@@ -2,58 +2,20 @@ import os
 from subprocess import check_call, CalledProcessError
 from tempfile import TemporaryDirectory, mkstemp
 from django.shortcuts import render, get_object_or_404
+from django.template.loader import render_to_string
 from algopedia.models import Algo, Implementation, Category, Star
 from pygments.formatters import LatexFormatter
 
 def generateTex(implementations):
     # TODO options de génération : titre, auteur, mutlicol?, linenos ?, a4paper/letter? marges? tailles, ordre des algos, sections
-    # TODO utiliser un template et render_to_string
-    # render_to_string('tex/notebook.tex', {'var': 'whatever'}))
-    # premise
-    latex = r"""\documentclass[8pt]{extarticle}
-\usepackage{fancyvrb}
-%\usepackage{listings}
-\usepackage{color}
-\usepackage[utf8]{inputenc}
-\usepackage[a4paper]{geometry}
-\geometry{
-  left=15mm,
-  right=7mm,
-  top=7mm,
-  bottom=15mm
-}
-%\usepackage{multicol}
-%\setlength{\columnsep}{1cm}
-%\def\@xobeysp{\mbox{}\space}
-"""
-    # pygments premise
-    latex += LatexFormatter().get_style_defs()
-    # title and table of contents
-    latex += r"""
-\title{Notebook}
-\author{}
-\date\today
-
-\begin{document}
-
-\maketitle
-%\begin{multicols}{2}
-\tableofcontents
-%\end{multicols}
-
-%\begin{multicols}{2}
-"""
-
-    for implem in implementations:
-        latex += "\subsection{" + implem.implementation.algo.name + "}\n" # TODO injection latex
-        latex += implem.implementation.get_code_tex()
-
-    # end
-    latex += r"""
-%\end{multicols}
-\end{document}
-"""
-    return latex
+    context = {
+      'style_defs': LatexFormatter().get_style_defs(), # pygments premise
+      'title': 'Notebook',
+      'author': 'Algopedia',
+      'stars': implementations,
+      'multicols': 1
+    }
+    return render_to_string('algopedia/notebook.tex', context)
 
 def generatePdf(implementations):
     """Returns the absolute pathname of the pdf file (to be deleted).
@@ -71,7 +33,7 @@ def generatePdf(implementations):
     os.close(texfile)
     # Compile the TeX file with PDFLaTeX
     for _ in range(3):
-        check_call(['pdflatex', texfilename, '-no-shell-escape']) # TODO environnement sécurisé
+        check_call(['pdflatex', texfilename, '-no-shell-escape', '-halt-on-error', '-interaction', 'errorstopmode']) # TODO environnement sécurisé
         # TODO catch CalledProcessError
     # Move resulting PDF
     os.rename(texfilename + '.pdf', pdffilename)
